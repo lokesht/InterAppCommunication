cfbf0cc1954f801ab323f588e350e0699a81c97d
Lokesh <tiwari9773@gmail.com>
2015-04-24 01:08:23 +0530

    Complited Process Client

--- ClientApp/src/com/example/clientapp/MainActivity.java
+++ ClientApp/src/com/example/clientapp/MainActivity.java
@@ -1,16 +1,43 @@
 package com.example.clientapp;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import android.app.Activity;
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+	private TextView text;
+	private EditText etMessageBox;
+
+	String packageName = "com.example.serverapp";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		init(savedInstanceState);
+	}
+
+	public void init(Bundle savedInstanceState) {
+		text = (TextView) findViewById(R.id.tv_show_text);
+		etMessageBox = (EditText) findViewById(R.id.et_get_name);
+
 	}
 
 	@Override
@@ -30,5 +57,56 @@ public class MainActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+
+	public void onGet(View v) {
+		loadFile();
+	}
+
+	private void readFile(String fullpath) {
+		File f = new File(fullpath);
+		FileInputStream fis = null;
+		try {
+			 fis = new FileInputStream(f);
+
+			int read = -1;
+			StringBuffer strBuff = new StringBuffer();
+			while ((read = fis.read()) != -1) {
+				strBuff.append((char) read);
+			}
+
+			etMessageBox.setText(strBuff);
+			text.setText("Found String");
+			
+		
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			text.setText(e.toString());
+		} catch (IOException e) {
+			e.printStackTrace();
+			text.setText(e.toString());
+		}
+		finally{
+			if(fis!=null)
+				try {
+					fis.close();
+				} catch (IOException e) {
+					text.setText(e.toString());
+					e.printStackTrace();
+				}
+		}
+
+	}
+
+	public void loadFile() {
+		PackageManager packageManage = getPackageManager();
+		try {
+			ApplicationInfo appInfo = packageManage.getApplicationInfo(packageName, PackageManager.GET_META_DATA);
+			String fullPath = appInfo.dataDir + "/files/server.text";
+			readFile(fullPath);
+		} catch (NameNotFoundException e) {
+			e.printStackTrace();
+			text.setText(e.toString());
+		}
 	}
 }
