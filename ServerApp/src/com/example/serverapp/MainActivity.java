8fa252ffea47d85d2a7ab3df0eee4872c1ebe4c3
Lokesh <tiwari9773@gmail.com>
2015-04-24 01:08:23 +0530

    Complited Process Client

--- ServerApp/src/com/example/serverapp/MainActivity.java
+++ ServerApp/src/com/example/serverapp/MainActivity.java
@@ -1,16 +1,35 @@
 package com.example.serverapp;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	private TextView text;
+	private EditText etMessageBox;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		init(savedInstanceState);
+	}
+
+	public void init(Bundle savedInstanceState) {
+		text = (TextView) findViewById(R.id.tv_show_text);
+		etMessageBox = (EditText) findViewById(R.id.et_add_name);
 	}
 
 	@Override
@@ -30,5 +49,34 @@ public class MainActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+
+	public void onSave(View v) {
+		writeFile();
+	}
+
+	public void writeFile() {
+		// File file = null;
+		FileOutputStream fos = null;
+		String status = etMessageBox.getText().toString();
+
+		try {
+			// file = getFilesDir();
+			fos = openFileOutput("server.text", Context.MODE_PRIVATE);
+
+			fos.write(status.getBytes());
+
+			text.setBackgroundColor(Color.GREEN);
+			text.setText(status + " Added TO file");
+
+		} catch (FileNotFoundException e) {
+			text.setBackgroundColor(Color.RED);
+			text.setText(e.toString());
+
+		} catch (IOException e) {
+			text.setBackgroundColor(Color.RED);
+			text.setText(e.toString());
+		}
+
 	}
 }
